0435a8bd769d227d8f3bfaf25d88a6208dfc3395
TundeOyinloye <sogsoggodsson@gmail.com>
2024-06-23 10:54:25 +0100

    Add project files.

---
+++ ECommerceAppOne/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ECommerceAppOne.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
